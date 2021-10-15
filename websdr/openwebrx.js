79d58e3f6241363e1a25f98b96aad80568ca5781
satcom-uhf <90838159+satcom-uhf@users.noreply.github.com>
2021-10-16 01:28:17 +0500

    init

--- websdr/openwebrx.js
+++ websdr/openwebrx.js
@@ -1038,7 +1038,6 @@ function getQueryVariable(variable)
        return(false);
 }
 function onTelegramAuth(user) {
-    $('#openwebrx-login-overlay').hide();
     window['tguser']=user;
     openwebrx_init();
     //console.warn('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
